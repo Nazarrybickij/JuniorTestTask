8b3299ca9039c1b114b1ac90f2da0fdc8fd4ad7b
Nazarrybickij <nazarrybickij@gmail.com>
2021-03-08 20:03:41 +0200

    little changes

--- app/src/main/java/com/nazarrybickij/juniortesttask/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/nazarrybickij/juniortesttask/viewmodel/MainViewModel.kt
@@ -3,24 +3,28 @@ package com.nazarrybickij.juniortesttask.viewmodel
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
+import androidx.lifecycle.viewModelScope
 import com.nazarrybickij.juniortesttask.network.ApiHelper
 import com.nazarrybickij.juniortesttask.network.NetworkRepository
 import com.nazarrybickij.juniortesttask.network.NetworkService
 import com.nazarrybickij.juniortesttask.utils.Resource
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
     private val networkRepository = NetworkRepository(ApiHelper(NetworkService.instance!!.apiService))
 
     @InternalCoroutinesApi
-    fun getUsers() = liveData(Dispatchers.IO) {
+    fun getUsers() = liveData {
         emit(Resource.loading())
-        try {
-            emit(Resource.success(networkRepository.getCars()))
-        } catch (e: Exception) {
-            emit(Resource.failed(e.message.toString()))
-            Log.e("ERROR:", e.message.toString())
+        viewModelScope.launch(Dispatchers.IO){
+            try {
+                emit(Resource.success(networkRepository.getCars()))
+            } catch (e: Exception) {
+                emit(Resource.failed(e.message.toString()))
+                Log.e("ERROR:", e.message.toString())
+            }
         }
     }
 }
